5b39ac79b3dd1ad68a5315e979015cb06926aed9
undernotic <undernotic@gmail.com>
2018-09-11 18:54:34 +0200

    benchmark

--- FastDictionaryComparer.Benchmark/Program.cs
+++ FastDictionaryComparer.Benchmark/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace FastDictionaryComparer.Benchmark
 {
+    [RankColumn]
+    public class FastDictionaryComparerBenchmark
+    {
+        private static Random random = new Random();
+        private FastAllDictionaryComparer<string, string> allDictionaryComparer;
+        private Dictionary<string, string>[] data;
+
+        [Params(1000, 10000)]
+        public int DictNumber;
+
+        [Params(5, 10)]
+        public int KeyValueStringLength;
+
+        [Params(5, 10)]
+        public int DictLength;
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            data = Enumerable.Range(0, DictNumber).Select(_ => Enumerable.Range(0, DictLength).ToDictionary(k => RandomString(KeyValueStringLength), v => RandomString(KeyValueStringLength))).ToArray();
+            allDictionaryComparer = FastAllDictionaryComparer<string, string>.Create(data);
+        }
+
+        [Benchmark]
+        public bool Contains() => data.Contains(new Dictionary<string, string> { { "12345", "12345" }, { "1234", "1234" }, { "123", "123" } });
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var summary = BenchmarkRunner.Run<FastDictionaryComparerBenchmark>();
         }
     }
 }
