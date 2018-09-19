b2cfb37006cbcdcce9477b0bd0c6cd9754e14ea9
undernotic <undernotic@gmail.com>
2018-09-19 10:18:48 +0200

    more tests

--- FastDictionaryComparer.Test/EquatableDictionaryOneOfTest.cs
+++ FastDictionaryComparer.Test/EquatableDictionaryOneOfTest.cs
@@ -8,22 +8,22 @@ namespace FastDictionaryComparer.Test
 {
     public class EquatableDictionaryOneOfTest
     {
-        private readonly IEnumerable<Dictionary<string, int>> _set;
+        private readonly IEnumerable<Dictionary<string, string>> _set;
 
         public EquatableDictionaryOneOfTest()
         {
-            _set = Enumerable.Range(0, 10).Select(i => new Dictionary<string, int> { { "key1", i }, { "key2", i } });
+            _set = Enumerable.Range(0, 10).Select(i => new Dictionary<string, string> { { "key1", i.ToString() }, { "key2", i.ToString() } });
         }
 
         [Fact]
         public void EquatableDictionaryOneOfShoulContain()
         {
-            var toFind1 = new Dictionary<string, int> { { "key1", 1 }, { "key3", 1 } };
-            var toFind2 = new Dictionary<string, int> { { "key1", 2 }, { "key2", 999 } };
-            var toFind3 = new Dictionary<string, int> { { "key1", 1 } };
-            var toFind4 = new Dictionary<string, int> { { "key2", 1 } };
+            var toFind1 = new Dictionary<string, string> { { "key1", "1" }, { "key3", "1" } };
+            var toFind2 = new Dictionary<string, string> { { "key1", "2" }, { "key2", "999" } };
+            var toFind3 = new Dictionary<string, string> { { "key1", "1" } };
+            var toFind4 = new Dictionary<string, string> { { "key2", "1" } };
 
-            var factory = new EquatableDictionaryFactory<string, int>(_set.Concat(new[] { toFind1, toFind2, toFind3, toFind4 }).ToArray());
+            var factory = new EquatableDictionaryFactory<string, string>(_set.Concat(new[] { toFind1, toFind2, toFind3, toFind4 }).ToArray());
 
             Assert.Contains(factory.CreateEquatableOneOfDictionary(toFind1), _set.Select(factory.CreateEquatableOneOfDictionary));
             Assert.Contains(factory.CreateEquatableOneOfDictionary(toFind2), _set.Select(factory.CreateEquatableOneOfDictionary));
@@ -34,25 +34,33 @@ namespace FastDictionaryComparer.Test
         [Fact]
         public void EquatableDictionaryOneOfShouldNotContain()
         {
-            var toFind1 = new Dictionary<string, int> { { "key1", 999 } };
-            var toFind2 = new Dictionary<string, int> { { "key3", 2 } };
+            var toFind1 = new Dictionary<string, string> { { "key1", "999" } };
+            var toFind2 = new Dictionary<string, string> { { "key3", "2" } };
 
-            var factory = new EquatableDictionaryFactory<string, int>(_set.Concat(new[] { toFind1, toFind2 }).ToArray());
+            var factory = new EquatableDictionaryFactory<string, string>(_set.Concat(new[] { toFind1, toFind2 }).ToArray());
 
             Assert.DoesNotContain(factory.CreateEquatableOneOfDictionary(toFind1), _set.Select(factory.CreateEquatableOneOfDictionary));
             Assert.DoesNotContain(factory.CreateEquatableOneOfDictionary(toFind2), _set.Select(factory.CreateEquatableOneOfDictionary));
         }
 
-         [Fact]
+        [Fact]
         public void EquatableDictionaryOneOfShouldUseKeyEqualityComparer()
         {
+            var toFind = new Dictionary<string, string> { { "KEY1", "1" } };
 
+            var factory = new EquatableDictionaryFactory<string, string>(_set.Concat(new[] { toFind }).ToArray(), keyEqualityComparer: StringComparer.InvariantCultureIgnoreCase);
+
+            Assert.Contains(factory.CreateEquatableOneOfDictionary(toFind), _set.Select(factory.CreateEquatableOneOfDictionary));
         }
 
         [Fact]
         public void EquatableDictionaryOneOfShouldUseValueEqualityComparer()
         {
+            var toFind = new Dictionary<string, string> { { "key1", "a" } };
 
+            var factory = new EquatableDictionaryFactory<string, string>(_set.Concat(new[] { toFind }).ToArray(), valueEqualityComparer: StringComparer.InvariantCultureIgnoreCase);
+
+            Assert.Contains(factory.CreateEquatableOneOfDictionary(toFind), _set.Select(factory.CreateEquatableOneOfDictionary).Concat(new[] { factory.CreateEquatableOneOfDictionary(new Dictionary<string, string> { { "key1", "A" }, { "key2", "A" } }) }));
         }
     }
 }
--- FastDictionaryComparer/EquatableDictionaryFactory.cs
+++ FastDictionaryComparer/EquatableDictionaryFactory.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using FastDictionaryComparer;
 
-
-// TO FACTORY FUNC???
 public class EquatableDictionaryFactory<T, Y>
 {
     private readonly T[] _allKeys;
+    private readonly IEqualityComparer<T> _keyEqualityComparer;
     private readonly Func<Y, int> _valueGetHashCode;
 
     public EquatableDictionaryFactory(Dictionary<T, Y>[] dicts, IEqualityComparer<T> keyEqualityComparer = null, IEqualityComparer<Y> valueEqualityComparer = null)
     {
         _allKeys = dicts.SelectMany(d => d.Keys).Distinct(keyEqualityComparer).OrderBy(_ => _).ToArray();
-
+        _keyEqualityComparer = keyEqualityComparer;
         if (valueEqualityComparer == null)
         {
             _valueGetHashCode = (x) => x.GetHashCode();
@@ -38,6 +37,9 @@ public class EquatableDictionaryFactory<T, Y>
 
     private (Dictionary<T, Y> dict, int?[] equatableValues) Create(Dictionary<T, Y> dict)
     {
+        if(_keyEqualityComparer != null){
+            dict = new Dictionary<T, Y>(dict, _keyEqualityComparer);
+        }
         var equatableValues = _allKeys.Select(key =>
         {
             if (dict.TryGetValue(key, out var value))
